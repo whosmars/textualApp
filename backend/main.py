c1d0cad12dcfca49f7c53a5dbfd84e13c94395f3
whosmars <mariosalazar@ciencias.unam.mx>
2026-05-08 20:02:28 -0600

    Add: Movie BaseModel

--- backend/main.py
+++ backend/main.py
@@ -14,11 +14,20 @@ def read_root():
 
 class Review(BaseModel):
     review_id : UUID = Field(default_factory=uuid4)
-    movie_title: str
-    liked : bool = False
+    liked : bool | None = None
     rating: Literal[0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
     comment: str | None = None
+    movie_id : UUID
     who_made_the_review : UUID
+
+class Movie(BaseModel):
+    movie_id : UUID = Field(default_factory=uuid4)
+    genre : Literal ["Thriller", "Suspense", "Horror", "Drama", "Comedy"] | None = None
+    movie_title : str
+    director : str | None = None
+    year : int | None = None
+    img: str | None = None
+
 
 class User(BaseModel):
     user_id : UUID = Field(default_factory=uuid4)
